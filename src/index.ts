1617d794c0b06d90e293eb3485c72af4baebce96
Rodolpho Reis <rodolphoreis@live.com>
2024-03-10 19:47:45 +0000

    feat: create get and post

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 import express from "express";
 import cors from "cors";
+import prisma from "../prisma/prisma-client";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/hotel", async (request, response) => {
+  try {
+    const reserve = await prisma.hotel.findMany();
+    return response.status(200).json({ message: "Reserva ok", error: false });
+  } catch (error) {
+    console.error("Erro ao efetuar reserva.", error);
+    return response.status(500).json({ message: error, error: true });
+  }
+});
+
+app.post("/hotel", async (request, response) => {
+  const { checkInDate, checkOutDate, name, numberOfGuests, roomType } =
+    request.body;
+  try {
+    const newReserve = await prisma.hotel.create({
+      data: {
+        checkInDate,
+        checkOutDate,
+        name,
+        numberOfGuests,
+        roomType,
+      },
+    });
+    return response
+      .status(201)
+      .json({ message: "Reserva efetuada com sucesso.", error: false });
+  } catch (error) {
+    console.error("Erro ao criar reserva.", error);
+    return response.status(500).json({ message: error, error: true });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}!`);
