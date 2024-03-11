5707deac0aee570585076bcd4bedc174af051d88
Rodolpho Reis <rodolphoreis@live.com>
2024-03-11 19:52:41 +0000

    feat: add routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes/routes";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}!`);
