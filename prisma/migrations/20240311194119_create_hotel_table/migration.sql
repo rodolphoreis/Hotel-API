13232cd703d7034b6fcd1c1a69db1a21eb6aa93f
Rodolpho Reis <rodolphoreis@live.com>
2024-03-11 19:52:24 +0000

    fix: change main migrate

--- prisma/migrations/20240311194119_create_hotel_table/migration.sql
+++ prisma/migrations/20240311194119_create_hotel_table/migration.sql
@@ -6,6 +6,8 @@ CREATE TABLE "Hotel" (
     "name" TEXT NOT NULL,
     "numberOfGuests" INTEGER NOT NULL,
     "roomType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Hotel_pkey" PRIMARY KEY ("id")
 );
